df154e7bbebe4771d1960e2f93fb9b04ca7cc791
Maajid Siddiqui <msiddiqui.digitals@gmail.com>
2021-03-15 01:14:26 +0500

    Project Setup and Small Components working

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+      {/* <Logo />
+      <ImageLinkForm />
+      <FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
